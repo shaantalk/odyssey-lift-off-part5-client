6bd223142b4c8458853a56bfa8dd92161024fc27
spanda <santanu.panda@gcp.lowes.com>
2022-01-03 23:37:41 +0530

    Added server url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://shaan-catstronauts.herokuapp.com/",
+  uri: "https://shaan-catstronauts-server.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
